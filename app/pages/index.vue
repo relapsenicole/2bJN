e1d6c6fc767a357c46ccf57e573bdb44a05a043b
mark joseph <tayagmj541@gmail.com>
2026-03-16 17:08:59 +0800

    added login page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,46 +1,31 @@
 <template>
-  <div>
-    <h1>Welcome To My Nuxt App</h1>
+  <div class="d-flex justify-center align-center" style="height: 90vh;">
+    <v-card class="text-center" width="500" elevation="12">
+      <v-icon size="100" class="mt-10">mdi-apple</v-icon>
+      <v-card-text class="pa-10">
+
+        <v-text-field 
+        prepend-inner-icon="mdi-account" 
+        variant="outlined" 
+        label="Username"></v-text-field>
+        <v-text-field 
+        prepend-inner-icon="mdi-lock"
+         variant="outlined" 
+         label="Password" 
+         type="password"></v-text-field>
+         <v-btn block color="black" @click="signIn">Sign in</v-btn>
+      </v-card-text>
+    </v-card>
   </div>
-
-  <v-row>
-    <v-col cols="12" md="3">
-      <v-card height="150" color="black">
-        <v-card-text>
-          <v-row no-gutter>
-            <v-col cols="6">
-              <v-icon size="100">mdi-currency-usd</v-icon>
-            </v-col>
-            <v-col cols="6">
-              <v-row>
-                <v-col cols="12" class="text-h4 text right">STUDENT</v-col>
-                <v-col cols="12" class="text-h3 text center">250</v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-col>
-
-    <v-col cols="12" md="3">
-      <v-card height="150" color="blue"></v-card>
-    </v-col>
-    <v-col cols="12" md="3">
-      <v-card height="150" color="green"></v-card>
-    </v-col>
-    <v-col cols="12" md="3">
-      <v-card height="150" color="yellow"></v-card>
-    </v-col>
-    <v-col cols="12" md="3">
-      <v-card height="150" color="pink"></v-card>
-    </v-col>
-    <v-col cols="12" md="3">
-      <v-card height="150" color="black"></v-card>
-    </v-col>
-  </v-row>
 </template>
-<script lang="ts" setup>
-</script>
+<script setup>
+definePageMeta({
+  layout: "auth"
+}
 
-<style>
-</style>
+);
+
+function signIn(){
+  alert("Successfully signed in!");
+}
+</script>
